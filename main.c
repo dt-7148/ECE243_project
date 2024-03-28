b5a6a366ab8bf4d908a9f3726c2cd0b478cd468f
Jonah Tang <70814302+dt-7148@users.noreply.github.com>
2024-03-28 13:33:26 -0400

    Update main.c
    Changed entity widths - going to be used for collision detection

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <math.h>
 
 
+
+
 #define PLAYER_VELO 5           // player velocity constant
 #define BULLET_VELO 20
 #define PLAYER_BULLET_VELO 20
@@ -12,6 +14,12 @@
 #define FIRE_RATE 10            // How many frames between an enemy shooting a bullet, lower = faster firing
 #define PLAYER_FIRE_RATE 10
 #define NUM_ENEMIES 5          // How many enemies to spawn in a given room
+
+#define PLAYER_WIDTH 5
+#define PLAYER_BULLET_WIDTH 3
+#define ENEMY_WIDTH 5
+#define ENEMY_BULLET_WIDTH 3
+
 
 
 void clear_screen();
@@ -30,6 +38,8 @@ void player_reset_bullet(int i);
 void init_enemies();
 
 
+
+
 int pixel_buffer_start; // global variable
 int back_buffer;
 int playerX;            // Player X position
@@ -46,6 +56,8 @@ int player_aim_h;       // Indicates whether the player is aiming horizontally/v
 int player_aim_v;       // player will shoot a bullet only when one of these is non-zero
 
 
+
+
 typedef struct entity {
     double x;
     double y;
@@ -55,9 +67,15 @@ typedef struct entity {
 } Entity;
 
 
+
+
 Entity enemyBullets[NUM_BULLETS];
 Entity playerBullets[NUM_BULLETS];
 Entity enemies[NUM_ENEMIES];
+
+
+
+
 
 
 
@@ -67,8 +85,12 @@ Entity enemies[NUM_ENEMIES];
 */
 
 
+
+
 int main(void)
 {
+
+
 
 
     playerX = playerY = 0;
@@ -79,6 +101,8 @@ int main(void)
     frameCounter_enemy = 0;
     frameCounter_player = 0;
    
+
+
 
 
     player_aim_h = player_aim_v = 0;
@@ -98,7 +122,11 @@ int main(void)
     }
 
 
+
+
 }
+
+
 
 
 void init_bullets() {
@@ -107,6 +135,8 @@ void init_bullets() {
         player_reset_bullet(i);
     }
 }
+
+
 
 
 void init_enemies() {
@@ -120,6 +150,8 @@ void init_enemies() {
 }
 
 
+
+
 void reset_bullet(int i) {
     enemyBullets[i].x = 100;
     enemyBullets[i].y = 100;
@@ -127,6 +159,8 @@ void reset_bullet(int i) {
     enemyBullets[i].velo_y = 0;
     enemyBullets[i].isActive = 0;
 }
+
+
 
 
 void player_reset_bullet(int i) {
@@ -138,16 +172,24 @@ void player_reset_bullet(int i) {
 }
 
 
+
+
 void handle_input() {
     unsigned char byte1 = 0;
     unsigned char byte2 = 0;
     unsigned char byte3 = 0;
 
 
+
+
     volatile int * PS2_ptr = (int *) 0xFF200100;  // PS/2 port address
 
 
+
+
     int PS2_data, RVALID;
+
+
 
 
     PS2_data = *(PS2_ptr);  // read the Data register in the PS/2 port
@@ -213,7 +255,11 @@ void handle_input() {
            
 
 
+
+
         // ==================================================== Arrow keys ====================================================
+
+
 
 
         case 0x75:                          // Up arrow
@@ -230,6 +276,8 @@ void handle_input() {
                 E0_FLAG = 0;
                 player_aim_v = -1;
             }
+
+
 
 
             break;
@@ -249,6 +297,8 @@ void handle_input() {
             }
 
 
+
+
             break;
         case 0x72:                          // Down arrow
             if (F0_FLAG) {
@@ -264,6 +314,8 @@ void handle_input() {
                 E0_FLAG = 0;
                 player_aim_v = 1;
             }
+
+
 
 
             break;
@@ -283,10 +335,16 @@ void handle_input() {
             }
 
 
+
+
             break;
 
 
+
+
         // =============================================== Special bytes ===============================================
+
+
 
 
         case 0xF0:
@@ -300,6 +358,8 @@ void handle_input() {
             break;
     }
 }
+
+
 
 
 void update_player() {
@@ -321,6 +381,8 @@ void update_player() {
 }
 
 
+
+
 void update_player_bullets() {
     if (frameCounter_player == PLAYER_FIRE_RATE) {
         // Fire bullet in aiming direction
@@ -337,6 +399,8 @@ void update_player_bullets() {
             player_curBullet = player_curBullet + 1;
 
 
+
+
            if (player_curBullet >= NUM_BULLETS) player_curBullet = 0;
         }
         else if (player_aim_v != 0) {
@@ -350,7 +414,11 @@ void update_player_bullets() {
             playerBullets[player_curBullet].isActive = 1;
 
 
+
+
             player_curBullet = player_curBullet + 1;
+
+
 
 
             if (player_curBullet >= NUM_BULLETS) player_curBullet = 0;
@@ -361,6 +429,8 @@ void update_player_bullets() {
     else {
         frameCounter_player = frameCounter_player + 1;
     }
+
+
 
 
     for (int i = 0; i < NUM_BULLETS; i++) {
@@ -377,7 +447,9 @@ void update_player_bullets() {
     }
 }
 
+
 // later: add a desync between enemies firing, don't make them all fire at the same time
+
 
 void update_bullet() {
     if (frameCounter_enemy == FIRE_RATE) {
@@ -392,6 +464,7 @@ void update_bullet() {
             enemyBullets[curBullet].velo_y = BULLET_VELO * (deltaY / hyp);
             enemyBullets[curBullet].isActive = 1;
             curBullet = curBullet + 1;
+
 
             if (curBullet >= NUM_BULLETS) curBullet = 0;
         }
@@ -417,6 +490,8 @@ void update_bullet() {
         }
     }
 }
+
+
 
 
 void update() {
@@ -445,6 +520,8 @@ void update() {
 }
 
 
+
+
 void draw() {
     clear_screen();
    
@@ -452,14 +529,14 @@ void draw() {
         depending on state, menu or game, draw something different
     */
    
-    for (int i = -5; i < 5; i++) {
-        for (int j = -5; j < 5; j++) {
+    for (int i = -PLAYER_WIDTH; i < PLAYER_WIDTH; i++) {
+        for (int j = -PLAYER_WIDTH; j < PLAYER_WIDTH; j++) {
             plot_pixel(playerX + i, playerY + j, 0xffff);  
         }
     }
    
-    for (int i = -5; i < 5; i++) {
-        for (int j = -5; j < 5; j++) {
+    for (int i = -ENEMY_WIDTH; i < ENEMY_WIDTH; i++) {
+        for (int j = -ENEMY_WIDTH; j < ENEMY_WIDTH; j++) {
             for (int k = 0; k < NUM_ENEMIES; k++) {
                 plot_pixel(enemies[k].x + i, enemies[k].y + j, 0x07e0);
             }  
@@ -467,8 +544,8 @@ void draw() {
     }
    
     for (int k = 0; k < NUM_BULLETS; k++) {
-        for (int i = -5; i < 5; i++) {
-            for (int j = -5; j < 5; j++) {
+        for (int i = -ENEMY_BULLET_WIDTH; i < ENEMY_BULLET_WIDTH; i++) {
+            for (int j = -ENEMY_BULLET_WIDTH; j < ENEMY_BULLET_WIDTH; j++) {
                 if (enemyBullets[k].isActive) {
                     plot_pixel(enemyBullets[k].x + i, enemyBullets[k].y + j, 0xfff1);  
                 }
@@ -477,15 +554,19 @@ void draw() {
     }
 
 
+
+
     for (int k = 0; k < NUM_BULLETS; k++) {
-        for (int i = -5; i < 5; i++) {
-            for (int j = -5; j < 5; j++) {
+        for (int i = -PLAYER_BULLET_WIDTH; i < PLAYER_BULLET_WIDTH; i++) {
+            for (int j = -PLAYER_BULLET_WIDTH; j < PLAYER_BULLET_WIDTH; j++) {
                 if (playerBullets[k].isActive) {
                     plot_pixel(playerBullets[k].x + i, playerBullets[k].y + j, 0x0011);
                 }
             }
         }
     }
+
+
 
 
     if (player_aim_h != 0) {
@@ -505,6 +586,8 @@ void draw() {
 }
 
 
+
+
 void waitForVsync() {
     volatile int *pixel_ctrl_ptr = (int *)0xFF203020;
     int status;
@@ -519,18 +602,30 @@ void waitForVsync() {
 
 
 
+
+
+
+
 void plot_pixel(int x, int y, short int line_color)
 {
      short int *one_pixel_address;
 
 
+
+
         one_pixel_address = back_buffer + (y << 10) + (x << 1);
+
+
 
 
         *one_pixel_address = line_color;
 
 
+
+
 }
+
+
 
 
 void clear_screen() {
@@ -540,6 +635,8 @@ void clear_screen() {
         }
     }
 }
+
+
 
 
 double squareRoot(double n, float l)
@@ -569,5 +666,11 @@ double squareRoot(double n, float l)
  
     return root;
 }
+
+
+
+
+
+
 
 
